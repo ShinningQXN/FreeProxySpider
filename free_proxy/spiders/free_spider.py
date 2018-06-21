9a1ee803389651891edfc5075253f362b2177225
Xiangning <xq11@rice.edu>
2018-06-21 17:32:54 -0500

    organized

--- free_proxy/spiders/free_spider.py
+++ free_proxy/spiders/free_spider.py
@@ -4,13 +4,11 @@ import re
 import logging
 import urllib.request as urllib2
 
-# OUTPUT_PATH = '../proxy_pool/free.json'
 
 
 # logger = logging.basicConfig(filename="./logs/free_spider_log.txt",level=logging.INFO)
-
 class ProxySpider(scrapy.Spider):
-    name = "free"
+    name = "ip_getter"
 
     custom_settings = {
         # 'ITEM_PIPELINES': {
@@ -19,94 +17,73 @@ class ProxySpider(scrapy.Spider):
 
         'FEED_URI': 'ips.json',
     }
-
-    #ITEM_PIPELINES = {
-#    'free_proxy.pipelines.FreeProxyPipeline': 300,
-#}
-
-    headers = {
-        "Content-Type": "application/x-www-form-urlencoded",
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
+    headers_proxy = {
+        'content-type':'application/x-www-form-urlencoded',
+        'user-agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'
     }
-    headers_arrow = {
-        "Host": "www.arrow.com",
-        "Connection": "keep-alive",
-        "Cache-Control": "max-age=0",
-        "Upgrade-Insecure-Requests": "1",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-        "DNT": "1",
-        "Accept-Encoding": "gzip, deflate, sdch",
-        "Accept-Language":"en-US,en;q=0.8",
-        "referer": "https://www.arrow.com"
-    }
+    
     headers_octo = {
-        "Host": "https://octopart.com/electronic-parts/integrated-circuits-ics",
-        "Connection": "keep-alive",
-        "Cache-Control": "max-age=0",
-        "Upgrade-Insecure-Requests": "1",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-        "DNT": "1",
-        "Accept-Encoding": "gzip, deflate, sdch",
-        "Accept-Language":"en-US,en;q=0.8",
-        "referer": "https://octopart.com/electronic-parts/integrated-circuits-ics"
+        'authority': 'octopart.com',
+        'method': 'GET',
+        'accept': 'application/json, text/javascript, */*; q=0.01',
+        'accept-encoding': 'gzip, deflate, br',
+        'accept-language': 'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7,la;q=0.6',
+        'referer': 'https://octopart.com/electronic-parts/integrated-circuits-ics',
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36',
+        'accept': '*/*',
+        'dnt': 1
     }
-
-
 
     proxys = []
 
-    # proxy = '185.93.3.123:8080'
+
     def start_requests(self):
-        # logging.info("Date: " + self.current_date)
-        open('./valid.txt', 'w').close()
-        request = Request(url= "https://free-proxy-list.net/", callback=self.parse, headers=self.headers)
-        
-        # request.meta['proxy'] = self.proxy
+        open('./ips.json', 'w').close()
+        request = Request(url= "https://free-proxy-list.net/", callback=self.parse, headers=self.headers_proxy)
         yield request
 
 
     def parse(self, response):
+        # parse free proxy list's response
         response.css('table.table-striped.table-bordered').css('tr')[1].css('td::text').extract()
         rows = response.css('table.table-striped.table-bordered').css('tr')
+        count = 0
+        al = []
+        # for each ip in the table
         for i in range(1, len(rows) - 1):
             item = rows[i].css('td::text').extract()
+            
             if item[6] == 'yes':
+                
                 ip = item[0]
                 port = item[1]
                 proxy = ip + ':' + port
 
-
-                
-                request = Request(url= "https://octopart.com/electronic-parts/integrated-circuits-ics", callback=self.parse_valid, headers=self.headers_arrow)
+                # filter the proxy, customize the url and header               
+                request = Request(url= "https://octopart.com/electronic-parts/integrated-circuits-ics", callback=self.parse_valid, headers=self.headers_octo, dont_filter=True)
                 request.meta['proxy'] = proxy
                 request.meta['qxn'] = proxy
-                # print ('f###########################################')
-                # proxy = urllib2.ProxyHandler({'http': curr_ip})
-                # opener = urllib2.build_opener(proxy)
-                # opener.addheaders = [('User-agent', 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36')]
-                # urllib2.install_opener(opener)
-                
+                al.append(ip)
                 try:
                     yield request                   
                 except:
+                    print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
                     continue
                     
-                yield {
-                    'ip': ip,
-                    'port': port
-                }
+                # yield {
+                #     'ip': ip,
+                #     'port': port
+                # }
                     # break
+            # print (len(al))
+            # print ("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
 
-    def parse_valid(self, response):
-        # print ('f###########################################')
+    def parse_valid(self, response):   
         ip = response.meta['qxn']
-        print ("###################################################3")
+        yield {'ip': ip}
+        print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
         print (ip)
-        # with open('./valid.txt', 'a+') as f:
-        #     f.write(proxy)
-        #     f.write("\n")
+
 
 
 
@@ -132,41 +109,6 @@ class ProxySpider(scrapy.Spider):
             self.logger.error('TimeoutError on %s', request.url)
 
 
-
-
-    # def parse(self, response):
-    #     response.css('table.table-striped.table-bordered').css('tr')[1].css('td::text').extract()
-    #     rows = response.css('table.table-striped.table-bordered').css('tr')
-    #     for i in range(1, len(rows) - 1):
-    #         item = rows[i].css('td::text').extract()
-    #         if item[6] == 'yes':
-    #             ip = item[0]
-    #             port = item[1]
-    #             proxy = ip + ':' + port
-                
-    #             request = Request(url= "https://www.octopart.com/", callback=self.parse_valid(proxy), headers=self.headers)
-    #             request.meta['proxy'] = proxy
-    #             yield request
-    #             # yield {
-    #             #     'ip': ip,
-    #             #     'port': port
-    #             # }
-
-    # def parse_valid(self, proxy):
-        
-    #     with open('./proxy_pool/valid.txt', 'a+') as f:
-    #         f.write(proxy)
-    #         f.write("\n")
-    # def parse_2(self, response):
-    #     print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-    #     print(response.css(".serp-part-card")[0].css(".offerRow").css('.col-seller-inner::text').extract_first())
-
-
-
-
-
-
-        
 
 
 
